4b47113b90d661031c9bd60c5f442e3fe4cd01f7
Rishi-737 <rishisunil777@gmail.com>
2026-04-15 09:43:09 +0530

    Delete SubmissionService.java

--- SubmissionService.java
+++
@@ -1,8 +0,0 @@
-class SubmissionService {
-public void submitCode(String code) {
-    System.out.println("Code submitted: " + code);
-}
-public void submitCode(String code, String language) {
-    System.out.println("Code submitted in " + language);
-}
-}
